98998e3b240be733975f512ea81e920b53c58f76
hectorBWA <hectorzavala12345@gmail.com>
2018-07-17 10:32:37 -0700

    portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showProjects: boolean;
+  showContact: boolean;
+  showHome: boolean;
+   constructor() {
+        this.showProjects = false;
+        this.showContact = false;
+        this.showHome = true;
+    }
+    ShowHomeButton() {
+      this.showProjects = false;
+      this.showContact = false;
+      this.showHome = true;
+    }
+    ShowProjectsButton() {
+      this.showProjects = true;
+        this.showContact = false;
+        this.showHome = false;
+  }
+  ShowContactButton() {
+    this.showProjects = false;
+        this.showContact = true;
+        this.showHome = false;
+}
 }
